9dc3fb29de0ce575e5fd92d484ba24a8a1530fe2
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-10 17:48:58 -0400

    moved setup metadata to other file

---
+++ CloudHarvestCoreDataModel/meta.py
@@ -0,0 +1,9 @@
+meta = dict(name="CloudHarvestCoreDataModel",
+            version="0.1.0",
+            description="This is the Core Data Model for CloudHarvest.",
+            author="Cloud Harvest, Fiona June Leathers",
+            license="CC Attribution-NonCommercial-ShareAlike 4.0 International",
+            url="https://github.com/Cloud-Harvest/CloudHarvestCoreDataModel",
+            classifiers=[
+                'Programming Language :: Python :: 3.12',
+            ])
--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup, find_packages
+from CloudHarvestCoreDataModel.meta import meta
 
 # Load requirements from requirements.txt
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-config = dict(name="CloudHarvestCoreDataModel",
-              version="0.1.0",
-              description="This is the Core Data Model for CloudHarvest.",
-              author="Cloud Harvest, Fiona June Leathers",
-              license="CC Attribution-NonCommercial-ShareAlike 4.0 International",
-              url="https://github.com/Cloud-Harvest/CloudHarvestCoreDataModel",
-              packages=find_packages(),
-              install_requires=requirements,
-              classifiers=[
-                  'Programming Language :: Python :: 3.12',
-              ])
+config = dict(packages=find_packages(),
+              install_requires=requirements)
+
+config = config | meta
 
 
 def main():
